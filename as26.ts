d3ff7d0f6a10767e1a6ceefe4402e385baf5d783
Muhammad Taha Bin Yamin <159868821+tahyami@users.noreply.github.com>
2024-05-27 21:09:37 +0500

    Delete as26.ts

--- as26.ts
+++
@@ -1,9 +0,0 @@
-let project : string[]=["Admin","user","user1","user2","user3","user4"];
-project.forEach(projectmanager=>{
-    if(projectmanager=="admin"){
-        console.log("Hello admin,welcome");
-    }
-    else{
-        console.log("Hello user,Enter Password to go");
-    }
-})
